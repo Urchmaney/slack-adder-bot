cc9b51d027c1830c9e7510f06402d57f33df35b6
Urchmaney <kingsobino@gmail.com>
2020-12-22 00:42:29 +0000

    seperate actions - create a new controller for register

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/bot/event', to: 'bot#create'
-  get '/bot/register', to: 'bot#register'
+  post '/bot/event', to: 'bot#listen'
+  get '/auth/register', to: 'auth#register'
 end
